50bcaed5227a073b5358b80345875f9352bdf8f5
Xxan1balknibalxX <anromudi@gmail.com>
2023-06-30 18:23:39 -0400

    ahora si

--- DjangoClase-main/newPage/urls.py
+++ DjangoClase-main/newPage/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("re4",views.bici1,name="bici1"),
+    path("Bici1",views.bici1,name="Bici1"),
     path("home",views.home,name="home"),
     path("contacto",views.contacto,name="contacto"),
     path("Bici4",views.bici4,name="Bici4"),
     path("Bici3",views.bici3,name="Bici3"),
     path("Bici2",views.bici2,name="Bici2"),
-    path("Bici5",views.bici5,name="bici5"),
+    path("Bici5",views.bici5,name="Bici5"),
     path("seguridad",views.seguridad,name="seguridad"),
     path("catalogo",views.catalogo,name="catalogo"),
     path("navbar",views.nav,name="navbar"),
